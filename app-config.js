2bc40b29ef91d29c1d516bea6cda9c036430f7ab
edudole <mintanaeducation@gmail.com>
2026-09-25 01:53:01 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 // LP360 District central configuration
 // เปลี่ยน Apps Script /exec ของเว็บไซต์อำเภอที่ไฟล์นี้เพียงจุดเดียว
 window.APP_CONFIG = Object.freeze({
-  EXEC_URL: 'https://script.google.com/macros/s/AKfycbyGRs8U6Y_90v4vp-b89DbPys6XdK10wNfk6wr9GlOodS56eCmt9mRAQaor06sPXSyw/exec',
+  EXEC_URL: 'https://script.google.com/macros/s/AKfycbzGOcbm-5oerlRH5T0z_HPn-wddQVySLeUqL29zObB8LcHmFTW7L101G3zxX_ucfX-y/exec',
   SITE_TYPE: 'DISTRICT',
   CACHE_PREFIX: 'LP360:DISTRICT:'
 });
