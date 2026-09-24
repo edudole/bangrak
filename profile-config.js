28a7c2558f1cec001a7f647e91fff778ebfb2205
edudole <mintanaeducation@gmail.com>
2026-09-25 02:08:51 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzXgrkVVpdpVYvg0pZnMY7S4RhAinhmX5jXeLwe32Yh8yc32wg7lPa3fZB5Oi06zIMe/exec';
+  'https://script.google.com/macros/s/AKfycbzKjAvy0NQXPI0vG-BkeAu1tmYEaooUHabIQUfwZ2Hn00prrOfCtLWHz6QewWA6qVPqgw/exec';
