6556563d4ab2b025e694697f70e63952c9969df4
edudole <mintanaeducation@gmail.com>
2026-09-25 10:31:48 +0700

    Update index-fast.js

--- suriyawong/index-fast.js
+++ suriyawong/index-fast.js
@@ -1803,7 +1803,7 @@ if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',
 
 /* ===== profile-config.js ===== */
 window.STUDENT_PROFILE_WEB_APP_URL =
-  '';
+  'https://script.google.com/macros/s/AKfycbwVcmoq6s7FIyjn8KRSoWbcsU936-HgTHGJAMeoP87y2amc__RGHbNPWyUUIwYhDtEH7A/exec';
 
 ;
 
