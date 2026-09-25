13d8cf31a035beb7c18e073172c310292486412f
edudole <mintanaeducation@gmail.com>
2026-09-25 10:22:41 +0700

    Update app-config.js

--- siphraya/app-config.js
+++ siphraya/app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycby7DjChYbHHeFr2aiPFzORgzGpTcsWmkyo80g7RXc3Vfmn7rV7lN5QhORBAgQpSNRmg/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycby4zrYBs2oTyzy_NdWxRimVFTEJUdvb6_TclonS4MlFLlVqmOdaYFucnVnXV3FaFPQs_A/exec'
   });
 })();
