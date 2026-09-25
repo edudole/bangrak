f65ef10f8ea2e1f2e25ee8436c8d5f235490b1bd
edudole <mintanaeducation@gmail.com>
2026-09-25 10:38:12 +0700

    Update index-fast.js

--- bang-rak/index-fast.js
+++ bang-rak/index-fast.js
@@ -1803,7 +1803,7 @@ if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',
 
 /* ===== profile-config.js ===== */
 window.STUDENT_PROFILE_WEB_APP_URL =
-  '';
+  'https://script.google.com/macros/s/AKfycbytWX0psf7c4a88mgfeDnnOy13x4KcPSpS32G66NTm32igiHb2y-c0QqzkrgWpf4l0J/exec';
 
 ;
 
