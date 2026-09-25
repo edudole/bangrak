a5cbd7ca4f27324f2cefefb1a45c2368e3a1b1d3
edudole <mintanaeducation@gmail.com>
2026-09-25 10:23:15 +0700

    Update index-fast.js

--- siphraya/index-fast.js
+++ siphraya/index-fast.js
@@ -1803,7 +1803,7 @@ if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',
 
 /* ===== profile-config.js ===== */
 window.STUDENT_PROFILE_WEB_APP_URL =
-  '';
+  'https://script.google.com/macros/s/AKfycbwptlgqNH1jg2NjwyeumhD6Mdtyx7niQo6L8wI8H_fMbbyflNx4IHbFwwGg8Su6VIlbPg/exec';
 
 ;
 
