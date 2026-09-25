06b867a9da395d2da9d2e878730e6241f71fd62b
edudole <mintanaeducation@gmail.com>
2026-09-25 10:11:57 +0700

    Update index-fast.js

--- mahaphruettharam/index-fast.js
+++ mahaphruettharam/index-fast.js
@@ -1803,7 +1803,7 @@ if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',
 
 /* ===== profile-config.js ===== */
 window.STUDENT_PROFILE_WEB_APP_URL =
-  '';
+  'https://script.google.com/macros/s/AKfycbzdAKhonGUj6_kCjxqaFC-C-ZpDWx1BDNLYB5Rr_PdnNgeNYshkUTu9dL7LP24Uu1PQlg/exec';
 
 ;
 
